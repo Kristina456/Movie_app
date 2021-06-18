57b1ba2f6ef0d4ec401bbd342d90d3cad0fad3c4
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-06-18 13:18:54 +0200

    Previous page, next page buttons

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { setConstantValue } from "typescript";
+import "./Movies.scss";
 
 export interface IMovieApiPage<T> {
   page: number;
@@ -24,15 +26,25 @@ export interface IMovieApiPopularResponse {
   vote_count: number;
 }
 
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+// const random = Math.floor(Math.random() * 440) + 1;
+
 export default function Movies() {
   const [response, setResponse] =
     useState<IMovieApiPage<IMovieApiPopularResponse>>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
 
-  const API_KEY = `${process.env.REACT_APP_API_KEY}`;
-  const random = Math.floor(Math.random() * 100) + 1;
+  useEffect(() => {
+    if (response) {
+      return;
+    }
+    getMovies(1);
+  }, [response]);
 
-  const getMovies = function () {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${random}`;
+  const getMovies = function (page: number) {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`;
+    setLoading(true);
     fetch(url)
       .then((response) => response.json())
       .then((json) => {
@@ -40,24 +52,40 @@ export default function Movies() {
         const result: IMovieApiPage<IMovieApiPopularResponse> = json;
         setResponse(result);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   };
 
-  useEffect(() => {
+  const loadNext = function () {
     if (response) {
-      return;
+      getMovies(response.page + 1);
     }
+  };
 
-    getMovies();
-  }, [response]);
+  const loadPrevious = function () {
+    if (response && response.page > 1) {
+      getMovies(response.page - 1);
+    }
+  };
+
+  if (loading) {
+    return <div>Loading page</div>;
+  }
+
+  if (error) {
+    return <div>Something went wrong!</div>;
+  }
 
   return (
     <div>
       <hr />
       <h1>Movie list</h1>
+      <button onClick={loadPrevious}>Previous Page</button>
+      <button onClick={loadNext}>Next Page</button>
       <hr />
-      {response?.results.map((item) => (
-        <div key={item.id}>
+      {error && <div>{error}</div>}
+      {response?.results.slice(0, 6).map((item) => (
+        <div className="wrapper" key={item.id}>
           <div>{item.vote_average}</div>
           <div>
             <img
@@ -65,11 +93,10 @@ export default function Movies() {
               alt={item.title}
             />
           </div>
-          <h1>
-            {item.original_title}
-            {item.release_date?.substring(0, 4)}
-          </h1>
+          <div>{item.release_date?.substring(0, 4)}</div>
+          <h1>{item.original_title}</h1>
           <div>Language: {item.original_language}</div>
+          <button>More info</button>
         </div>
       ))}
     </div>
